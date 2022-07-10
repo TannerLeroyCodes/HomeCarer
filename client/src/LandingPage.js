771e4d6d9121bb4b5cd0f18a4aac7df0c7d9f767
tannerleroy <tanner@tleroy.com>
2022-07-10 18:58:48 -0400

    styling

--- client/src/LandingPage.js
+++ client/src/LandingPage.js
@@ -4,7 +4,7 @@ import logo from "./logo.jpg"
 
 function LandingPage() {
   return (<div className="landing">
-    <h1> <img src={logo} className="logo" alt="logo"></img>HomeCarer </h1>
+    <h1> <img src={logo} className="logo-landing" alt="logo"></img>HomeCarer </h1>
     
     <h3>Committed to imrpoving the in-home healthcare experience for those in need of care and for those providing it</h3>
 
